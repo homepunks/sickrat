a58c3a36fee1fb9b8593edb9dc2a3d9d1c72d973
homepunks <wispenjoyer@gmail.com>
2025-12-09 16:47:50 +0500

    Refactor code

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,27 @@
 use std::fs;
 use std::thread;
 use std::time::Duration;
-use std::error::Error;
 use std::env::home_dir;
 
 use copypasta::{ClipboardContext, ClipboardProvider};
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let path = home_dir()
-	.ok_or("home directory not found!")?
-	.join("Programming/token.txt");
-    
-    let content = fs::read_to_string(&path)
-	.map_err(|e| format!("couldn't read {}: {}", path.display(), e))?;
+type Result<T> = std::result::Result<T, ()>;
 
+fn main() -> Result<()> {
+    let secret_path = match home_dir() {
+	Some(home) => home.join("Secret/very_secret_password.txt"),
+	None => {
+	    eprintln!("ERROR: Home directory not found.");
+	    std::process::exit(69);
+	}
+    };
+    
+    let content = fs::read_to_string(&secret_path)
+	.map_err(|e| eprintln!("ERROR: Couldn't read {}: {}", secret_path.display(), e))?;
     let mut ctx = ClipboardContext::new()
-	.map_err(|e| format!("clipboard error: {e}"))?;
+	.map_err(|e| eprintln!("ERROR: Clipboard error: {e}"))?;
     ctx.set_contents(content.clone())
-	.map_err(|e| format!("clipboard copying error: {e}"))?;
+	.map_err(|e| eprintln!("ERROR: Clipboard copying error: {e}"))?;
 
     thread::sleep(Duration::from_secs(5));
     
